cf1240482f48798cb339f20faf3e8f9f612801fb
hiro1125 <hiro112538@gmail.com>
2022-11-14 20:08:20 +0900

    Header addition

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import "../styles/header.css";
+import { headerData } from "../contents";
+
+const Header = () => {
+  return (
+    <header>
+      <ul>
+        {headerData.map((item, key) => {
+          return (
+            <li key={key}>
+              <a href="">{item.listName}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
