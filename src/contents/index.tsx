34a09ef7f1561ce9f7332f23108bb230ee664d6c
hiro1125 <hiro112538@gmail.com>
2022-11-14 20:09:08 +0900

    Header addition

---
+++ src/contents/index.tsx
@@ -0,0 +1,21 @@
+export const headerData = [
+  {
+    listName: `home`,
+  },
+
+  {
+    listName: `About`,
+  },
+
+  {
+    listName: `Skill`,
+  },
+
+  {
+    listName: `Product`,
+  },
+
+  {
+    listName: `Contact`,
+  },
+];
