9fcfd5b31ca3e1705257c47869962ec112e4da23
hiro1125 <hiro112538@gmail.com>
2022-11-15 20:29:26 +0900

    Add text to the About screen

--- src/components/TopAbout.tsx
+++ src/components/TopAbout.tsx
@@ -13,6 +13,11 @@ const TopAbout = () => {
             {
               "1997年生まれ、兵庫県出身の24歳。 \n 歩くことが好きです。 \n 卒業研究で、Octaveを触ってプログラミングが好きになりました。"
             }
+            <div className="topAbout-viewMore">
+              <a target="_blank" href="about.html" className="link">
+                view more
+              </a>
+            </div>
           </div>
         </div>
       </div>
