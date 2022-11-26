8c059691d3fa276059888de6fce7f4f3eeba8e89
hiro1125 <hiro112538@gmail.com>
2022-11-27 02:14:42 +0900

    Adding SkillData

---
+++ src/contents/index.ts
@@ -0,0 +1,53 @@
+export const headerData = [
+  {
+    listName: 'home',
+  },
+
+  {
+    listName: 'About',
+  },
+
+  {
+    listName: 'Skill',
+  },
+
+  {
+    listName: 'Product',
+  },
+
+  {
+    listName: 'Contact',
+  },
+];
+
+export const SkillData = [
+  {
+    skillIcon: require('../images/html-5.png'),
+    skillName: 'HTML',
+  },
+
+  {
+    skillIcon: require('../images/css-3.png'),
+    skillName: 'CSS',
+  },
+
+  {
+    skillIcon: require('../images/javascript.png'),
+    skillName: 'Javascript',
+  },
+
+  {
+    skillIcon: require('../images/react.png'),
+    skillName: 'React',
+  },
+
+  {
+    skillIcon: require('../images/github-icon.png'),
+    skillName: 'Github',
+  },
+
+  {
+    skillIcon: require('../images/expo-icon.png'),
+    skillName: 'expo',
+  },
+];
