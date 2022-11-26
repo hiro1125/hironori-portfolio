f5e24bc0557cdbf58d3ff8399bdf36ec7dad0e5d
hiro1125 <hiro112538@gmail.com>
2022-11-27 02:15:42 +0900

    Loading png images

---
+++ src/images/index.ts
@@ -0,0 +1,7 @@
+export { default as topMain } from "./topMain.png";
+export { default as topAbout } from "./topAbout-icon.png";
+export { default as htmlIcon } from "./html-5.png";
+export { default as cssIcon } from "./css-3.png";
+export { default as javascriptIcon } from "./javascript.png";
+export { default as reactIcon } from "./react.png";
+export { default as githubIcon } from "./github-icon.png";
