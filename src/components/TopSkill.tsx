af04fb0cdc0be53fff6854d3248790eeb7e2a005
hiro1125 <hiro112538@gmail.com>
2022-11-27 02:14:10 +0900

    Change TopSkill

--- src/components/TopSkill.tsx
+++ src/components/TopSkill.tsx
@@ -1,12 +1,26 @@
-import { headerData } from "../contents";
-import "../styles/topAbout.css";
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { headerData, SkillData } from '../contents';
+import '../styles/topSkill.css';
 
 const TopSkill = () => {
   return (
     <>
-      <div className="title">
+      <div className='title'>
         {headerData[2].listName}
-        <div className="area"></div>
+        <div className='area skillArea'>
+          {SkillData.map((item, key) => {
+            return (
+              <li className='skillItem' key={key}>
+                <img
+                  className='iconImg'
+                  src={item.skillIcon}
+                  alt={item.skillName}
+                />
+                <p className='skillName'>{item.skillName}</p>
+              </li>
+            );
+          })}
+        </div>
       </div>
     </>
   );
