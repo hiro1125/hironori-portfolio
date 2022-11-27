deda15737a69acc387b54de4f870d45c72328009
hiro1125 <hiro112538@gmail.com>
2022-11-27 18:09:36 +0900

    Add product screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import "./App.css";
-import Header from "./components/Header";
-import TopMain from "./components/TopMain";
-import TopAbout from "./components/TopAbout";
-import TopSkill from "./components/TopSkill";
+import './App.css';
+import Header from './components/Header';
+import TopMain from './components/TopMain';
+import TopAbout from './components/TopAbout';
+import TopSkill from './components/TopSkill';
+import TopProduct from './components/TopProduct';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <div>
         <Header />
         <TopMain />
         <TopAbout />
         <TopSkill />
+        <TopProduct />
       </div>
     </div>
   );
---
+++ src/components/TopProduct.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { headerData, ProductData } from '../contents';
+import '../styles/topProduct.css';
+
+const TopProduct = () => {
+  return (
+    <>
+      <div className='title'>
+        {headerData[3].listName}
+        <div className='area'>
+          <div className='area skillArea'>
+            {ProductData.map((item, key) => {
+              return (
+                <li className='skillItem' key={key}>
+                  <img
+                    className='ProductImg'
+                    src={item.ProductImg}
+                    alt={item.ProductName}
+                  />
+                  <p className='ProductName'>{item.ProductName}</p>
+                </li>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TopProduct;
