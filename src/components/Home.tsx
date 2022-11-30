22b8c68e604972df9fa9bc423ecafa86a851e53d
hiro1125 <hiro112538@gmail.com>
2022-11-30 21:18:16 +0900

    Home screen added

---
+++ src/components/Home.tsx
@@ -0,0 +1,19 @@
+import Header from './Header';
+import TopAbout from './TopAbout';
+import TopMain from './TopMain';
+import TopProduct from './TopProduct';
+import TopSkill from './TopSkill';
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <TopMain />
+      <TopAbout />
+      <TopSkill />
+      <TopProduct />
+    </>
+  );
+}
+
+export default Home;
