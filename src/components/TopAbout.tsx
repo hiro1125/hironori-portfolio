ab15f967eb48a8cfdb3f7434bed5dc870d36945a
hiro1125 <hiro112538@gmail.com>
2022-11-30 21:19:27 +0900

    Adding NavLink

--- src/components/TopAbout.tsx
+++ src/components/TopAbout.tsx
@@ -1,22 +1,29 @@
-import { headerData } from "../contents";
-import { topAbout } from "../images";
-import "../styles/topAbout.css";
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+import { NavLink } from 'react-router-dom';
+import { navData, navPathData } from '../contents';
+import { topAbout } from '../images';
+import '../styles/topAbout.css';
 
 const TopAbout = () => {
   return (
     <>
-      <div className="title">
-        {headerData[1].listName}
-        <div className="area">
-          <img className="topAbout-icon" src={topAbout} alt="" />
-          <div className="topAbout-txt">
+      <div className='title'>
+        {navData[1].listName}
+        <div className='area'>
+          <img className='topAbout-icon' src={topAbout} alt='' />
+          <div className='topAbout-txt'>
             {
-              "1997年生まれ、兵庫県出身の24歳。 \n 歩くことが好きです。 \n 卒業研究で、Octaveを触ってプログラミングが好きになりました。"
+              '1997年生まれ、兵庫県出身の24歳。 \n 歩くことが好きです。 \n 卒業研究で、Octaveを触ってプログラミングが好きになりました。'
             }
-            <div className="topAbout-viewMore">
-              <a target="_blank" href="about.html" className="link">
+            <div className='topAbout-viewMore'>
+              <NavLink
+                className='navLink nav-item'
+                target='_blank'
+                to={navPathData.about}
+              >
                 view more
-              </a>
+              </NavLink>
             </div>
           </div>
         </div>
