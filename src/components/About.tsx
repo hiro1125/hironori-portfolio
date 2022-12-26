bb294d2ba13a4dd976a815d7db0537a724a8da41
hiro1125 <hiro112538@gmail.com>
2022-12-26 21:33:36 +0900

    Implementing react-vertical-timeline

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,54 +1,73 @@
-import { CareerData, navData, navPathData } from '../contents';
+import { careerData, navData, navPathData } from '../contents';
 import { about, headerLogo } from '../images';
 import '../styles/about.css';
 import Footer from './Footer';
-import { animateScroll as scroll } from 'react-scroll';
+// import { animateScroll as scroll } from 'react-scroll';
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { FaHospital } from 'react-icons/fa';
+import TopContact from './TopContact';
 
-const scrollToTop = () => {
-  scroll.scrollToTop();
-};
+// const scrollToTop = () => {
+//   scroll.scrollToTop();
+// };
 
 const About = () => {
   return (
     <>
+      <img className='headerLogo' src={headerLogo} alt='ロゴ画像' />
       <div className='area'>
-        <section id='top'></section>
-        <img
-          className='headerLogo'
-          src={headerLogo}
-          alt='ロゴ画像'
-          onClick={scrollToTop}
-        />
-        <div className='title'>
-          {navData[1].listName}
-          <img className='about-icon' src={about} alt='' />
-          <div className='topAbout-txt'>
-            <p className='paragraph'>
-              2021年から病院の仕事をしながらプログラミングの勉強を独学を始めました。
-            </p>
-            <p className='paragraph'>
-              現在は、Reactを中心に行い、UdemyやYouTubeを通して日々学んでいます。
-            </p>
+        <div className='title aboutTitle'>
+          <section id='top'>{navData[1].listName}</section>
+          <div className='AboutMain'>
+            <img className='about-icon' src={about} alt='' />
+            <div className='topAbout-txt'>
+              <p className='paragraph'>
+                2021年から病院の仕事をしながらプログラミングの勉強を独学を始めました。
+              </p>
+              <p className='paragraph'>
+                現在は、Reactを中心に行い、UdemyやYouTubeを通して日々学んでいます。
+              </p>
+            </div>
           </div>
         </div>
       </div>
-      <div className='title'>
-        Career
-        <div className='area careerArea'>
-          {CareerData.map((item, key) => {
-            return (
-              <dl className='career' key={key}>
-                <dt className='careerLeft'>{item.lineDate}</dt>
-                <dd className='careerRight'>
-                  <span className='careerHeadline'>{item.titleData}</span>
-                  <br />
-                  {item.textData}
-                </dd>
-              </dl>
-            );
-          })}
+      <div className='area careerArea'>
+        <div className='title careerTitle'>
+          <h1>Career</h1>
+          <VerticalTimeline lineColor='#000'>
+            {careerData.map((item, key) => {
+              return (
+                <VerticalTimelineElement
+                  className='vertical-timeline-element--work'
+                  key={key}
+                  contentStyle={{
+                    background: 'rgb(33, 150, 243)',
+                    color: '#fff',
+                  }}
+                  contentArrowStyle={{
+                    borderRight: '7px solid  rgb(33, 150, 243)',
+                  }}
+                  date={item.lineDate}
+                  dateClassName={'timelineDate'}
+                  iconStyle={{
+                    background: 'rgb(33, 150, 243)',
+                    color: '#fff',
+                  }}
+                  icon={<FaHospital />}
+                >
+                  <h3>{item.titleData}</h3>
+                  <p>{item.textData}</p>
+                </VerticalTimelineElement>
+              );
+            })}
+          </VerticalTimeline>
         </div>
       </div>
+      <TopContact />
       <div className='backHome'>
         <a href={navPathData.home}>← back home</a>
       </div>
