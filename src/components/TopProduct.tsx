ca1418a6e3e84828f5cf7ab3bc19750267e9c802
hiro1125 <hiro112538@gmail.com>
2023-09-20 22:28:24 +0900

    style: TopProductの画像の間隔調整
    それに伴い、divタグを追加

--- src/components/TopProduct.tsx
+++ src/components/TopProduct.tsx
@@ -8,25 +8,28 @@ const TopProduct = () => {
       <div className='area topProductArea'>
         <div className='title'>
           <section id='topProduct'>{navData[3].listName}</section>
-          {productData.map((item, key) => {
-            return (
-              <li className='topProductList' key={key}>
-                <a
-                  className='ProductImg'
-                  href={item.productLink}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <img
+          <div className='topProductMain'>
+            {productData.map((item, key) => {
+              return (
+                <li className='topProductList' key={key}>
+                  <a
                     className='ProductImg'
-                    src={item.productImg}
-                    alt={item.productName}
-                  />
-                </a>
-                <p className='ProductName'>{item.productName}</p>
-              </li>
-            );
-          })}
+                    href={item.productLink}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    <img
+                      className='ProductImg'
+                      src={item.productImg}
+                      alt={item.productName}
+                    />
+                  </a>
+                  <p className='ProductName'>{item.productName}</p>
+                </li>
+              );
+            })}
+            <div className='EmptyBox'></div>
+          </div>
         </div>
       </div>
     </>
